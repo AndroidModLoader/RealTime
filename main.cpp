30f2482d3554e8dedb646cfac04f475ead72ffe1
RusJJ <mctesting1@yandex.ru>
2022-12-01 11:19:06 +0300

    VC fix

--- main.cpp
+++ main.cpp
@@ -12,9 +12,10 @@ void* hGame;
 
 // Game Vars
 char *ms_nGameClockHours, *ms_nGameClockMinutes, *ms_nGameClockSeconds;
+unsigned int *ms_nMillisecondsPerGameMinute;
 
 // Hooks
-DECL_HOOK(void, ClockUpdate, void* self)
+DECL_HOOKv(ClockUpdate, void* self)
 {
     time_t now = time(NULL);
     struct tm* t = localtime(&now);
@@ -36,19 +37,29 @@ DECL_HOOK(void, ClockUpdate, void* self)
         *ms_nGameClockSeconds = (char)(t->tm_sec);
     }*/
 }
-DECL_HOOK(void, ClockInit, unsigned int msPerMin)
+DECL_HOOKv(ClockInit, unsigned int msPerMin)
 {
     ClockInit(60000); // I was so lazy to patch it so i just hooked it...
+}
+DECL_HOOKv(GenericLoad)
+{
+    GenericLoad();
+    *ms_nMillisecondsPerGameMinute = 60000;
 }
 
 extern "C" void OnModLoad()
 {
     hGame = dlopen("libGTASA.so", RTLD_LAZY);
-    if(hGame == NULL) hGame = dlopen("libGTAVC.so", RTLD_LAZY);
+    if(hGame == NULL)
+    {
+        hGame = dlopen("libGTAVC.so", RTLD_LAZY);
+        HOOK(GenericLoad,        aml->GetSym(hGame, "_Z11GenericLoadv"));
+    }
 
     HOOK(ClockUpdate,            aml->GetSym(hGame, "_ZN6CClock6UpdateEv"));
     HOOK(ClockInit,              aml->GetSym(hGame, "_ZN6CClock10InitialiseEj"));
     SET_TO(ms_nGameClockHours,   aml->GetSym(hGame, "_ZN6CClock18ms_nGameClockHoursE"));
     SET_TO(ms_nGameClockMinutes, aml->GetSym(hGame, "_ZN6CClock20ms_nGameClockMinutesE"));
     SET_TO(ms_nGameClockSeconds, aml->GetSym(hGame, "_ZN6CClock20ms_nGameClockSecondsE"));
+    SET_TO(ms_nMillisecondsPerGameMinute, aml->GetSym(hGame, "_ZN6CClock29ms_nMillisecondsPerGameMinuteE"));
 }
